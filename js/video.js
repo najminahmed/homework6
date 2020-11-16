70201f2e4b63e72691d31a3ce56a9b1d2182fce5
najminahmed <49462221+najminahmed@users.noreply.github.com>
2020-11-15 20:50:58 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -39,3 +39,11 @@ document.querySelector("#mute").addEventListener("click", function() {
         video.muted = false;
     }
 });
+
+document.querySelector("#old").addEventListener("click", function() {
+	video.classList.add("oldTime");
+});
+
+document.querySelector("#original").addEventListener("click", function() {
+	video.classList.remove("oldTime");
+});
