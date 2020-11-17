4af6c8fe9c224d145a4acd64d86aed5812bb6620
najminahmed <49462221+najminahmed@users.noreply.github.com>
2020-11-17 17:19:18 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
-	let vol= document.querySelector("#volume").innerHTML = video.volume*100 + '%';
+	//let vol= document.querySelector("#volume").innerHTML = video.volume*100 + '%';
 	
 });
 
